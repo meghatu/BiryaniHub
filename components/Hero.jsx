a0ec991c2d427cf1c1d803ea4135bc8c3a63b5bd
Megha V <meghavayath2001@gmail.com>
2023-03-08 14:07:47 +0530

    modification of footer

---
+++ components/Hero.jsx
@@ -0,0 +1,9 @@
+import css from '../styles/Hero.module.css'
+export default function Hero(){
+    return(
+        <div className={css.container}>
+            {/* left side */}
+            {/* right side */}
+        </div>
+    )
+};
