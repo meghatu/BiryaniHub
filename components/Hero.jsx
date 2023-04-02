6086939c8924df20bf1cb726662746b19c4dcc78
Megha V <meghavayath2001@gmail.com>
2023-04-02 15:02:57 +0530

    forenoon

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,38 +1,51 @@
-import Image from 'next/image'
-import css from '../styles/Hero.module.css'
-import Tasty from '../assets/Tasty.png'
+import css from '../styles/Hero.module.css';
+import Image from 'next/image';
+import Tasty from "../assets/Tasty.png";
+import HeroImage from "../assets/HeroImage.png";
+import {UilPhone} from "@iconscout/react-unicons";
+import Biryani1 from '../assets/p1.jpg'
 
-export default function Hero(){
+export default function Hero() {
     return(
         <div className={css.container}>
             {/* left side */}
             <div className={css.left}>
                 <div className={css.cherryDiv}>
-                   <span>Biryani Bliss</span>
-                   <Image src={Tasty} alt="" width={40} height={25}/>
+                    <span>Biryani Bliss</span>
+                    <Image src={Tasty} alt="" width={30} height={30}/>
                 </div>
-            
-
             <div className={css.heroText}>
-                <span>Be the Fastest</span>
-                <span>In Delivery</span>
+                <span>Biryani Delivered</span>
+                <span>Faster Than You</span>
                 <span>
-                    Your <span style={{color: "var(--themeRed)"}}>Biryani</span>
+                    Can Say <span style={{color: "var(--themeRed)"}}>'Yum!'</span>
                 </span>
             </div>
-
-            <span className={css.miniText}>
-                Our Mission is to filling your tummy with delicious food and with free delivery
-            </span>
-
-            <button className={'btn ${css.btn}'}>
-                Get Started
-            </button>
-            </div>
+            <span className={css.miniText}> Our mission is to filling you tummy with delicious food and with fast and free delivery</span>
+            <button className={`btn ${css.btn}`}>Order Now</button>
+        </div>
             {/* right side */}
+            <div className={css.right}>
+                <div className={css.imageContainer}>
+                    <Image src={HeroImage} alt="" layout="intrinsic"/>
+                </div>
+                <div className={css.ContactUs}>
+                    <span>Contact Us</span>
+                    <div>
+                        <UilPhone color='white'/>
+                    </div>
+                </div>
 
-            <div className={css.rightSide}>
-                21
+                <div className={css.Biryani}>
+                    <div>
+                        <Image src={Biryani1} alt="" object="cover" layout="intrinsic"/>
+                    </div>
+                    <div className={css.details}>
+                        <span>Thalassery Dum Biryani</span>
+                        <span>
+                            <span style={{color: "var(--themeRed)"}}>₹</span> 149</span>
+                    </div>
+                </div>
             </div>
         </div>
     )
