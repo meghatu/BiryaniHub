776b5460698f522e44aa6c345203003b2979e620
praneethtt <praneethtt@genproresearch.net>
2023-04-09 19:04:09 +0530

    admin login sample

---
+++ pages/admin/index.jsx
@@ -0,0 +1,14 @@
+import styles from "../../styles/Admin.module.css"
+import Layout from "../../components/Layout";
+
+
+const index = () => {
+    return (
+        <Layout>
+            <div className={styles.container}></div>
+        </Layout>
+
+    )
+}
+
+export default index
